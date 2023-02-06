b0ba9a8a9e09abe04a1040054d879942d72a9182
冉运财 <2837246896@qq.com>
2023-02-06 17:02:51 +0800

    Initial commit

---
+++ src/main/java/com/redtv/redtvwebserve/RedTvWebServeApplication.java
@@ -0,0 +1,13 @@
+package com.redtv.redtvwebserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedTvWebServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedTvWebServeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redtv/redtvwebserve/RedTvWebServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redtv.redtvwebserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedTvWebServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
